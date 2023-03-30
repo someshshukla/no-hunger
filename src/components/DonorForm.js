80385c7781125acd71c413e21d217e922c95db5b
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-30 16:33:12 +0530

    some stuff

--- src/components/DonorForm.js
+++ src/components/DonorForm.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
+
 const DonorForm = () => {
+  const data={name:"", phone:"", aadhar:"", password:""}
   return (
+
     <form className="container">
         <div className="header">
-            <h1>Donater Form</h1>
+            <h1>Donor Form</h1>
+        </div>
+        <div>
+        <input type="text" placeholder='Your Full Name' name='name'/>
+        </div>
+        <div>
+        <input type="text" placeholder='Your Phone Number' phone='phone'/>
+        </div>
+        <div>
+        <input type="text" placeholder='Your Aadhar Number' aadhar='aadhar'/>
+        </div>
+        <div>
+        <input type="text" placeholder='Your password' password='password'/>
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
         </div>
     </form>
   )
